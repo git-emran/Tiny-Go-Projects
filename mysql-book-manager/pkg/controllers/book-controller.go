6a7851df5e503dfcb587dd484b53b22c6d3b973a
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-30 15:42:45 +0600

    Creating controllers, Only update books remaining

--- mysql-book-manager/pkg/controllers/book-controller.go
+++ mysql-book-manager/pkg/controllers/book-controller.go
@@ -1,1 +1,62 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/git-emran/mysql-bookmanager/pkg/models"
+	"github.com/git-emran/mysql-bookmanager/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+
+	bookDtails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDtails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	book := models.DeleteBookId(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
