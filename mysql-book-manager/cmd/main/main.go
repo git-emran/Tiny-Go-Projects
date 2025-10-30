f94eff8cd1f708ddcdd6a30cdb2c6efeb6a75296
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-30 13:56:56 +0600

    main.go, models and utils

--- mysql-book-manager/cmd/main/main.go
+++ mysql-book-manager/cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/git-emran/mysql-bookmanager/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+}
--- mysql-book-manager/pkg/models/book.go
+++ mysql-book-manager/pkg/models/book.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"github.com/git-emran/mysql-bookmanager/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
