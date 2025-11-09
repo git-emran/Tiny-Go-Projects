aab490fa8c23803168a955241c4293f507feab41
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-11-09 09:43:28 +0600

    serving static templates

--- serving-templates/first-template.go
+++ serving-templates/first-template.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func renderTemplate(w http.ResponseWriter, r *http.Request) {
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+)
 
+type Person struct {
+	ID   string
+	Name string
+}
+
+func renderTemplate(w http.ResponseWriter, r *http.Request) {
+	person := Person{ID: "1", Name: "Foo"}
+	parsedTemplate, _ := template.ParseFiles("templates/first-template.html")
+	err := parsedTemplate.Execute(w, person)
+	if err != nil {
+		log.Printf("Error occured while executing the template %v", err)
+		return
+	}
 }
 
 func main() {
 
+	http.HandleFunc("/", renderTemplate)
+	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
+	if err != nil {
+		log.Fatal("error starting http server", err)
+		return
+	}
 }
