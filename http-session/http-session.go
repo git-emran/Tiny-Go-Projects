c8721427b3060c481ebf9cfbd78104b6821c56ee
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-11-17 22:00:32 +0600

    scaffolding http session

--- http-session/http-session.go
+++ http-session/http-session.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gorilla/sessions"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
 
 const (
 	CONN_HOST = "localhost"
@@ -11,4 +16,28 @@ var store *sessions.CookieStore
 
 func init() {
 	store = sessions.NewCookieStore([]byte("secret-key"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	session, _ := store.Get(r, "session-name")
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+	fmt.Fprintln(w, "you have successfully logged in")
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+
 }
