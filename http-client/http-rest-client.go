7de5e52841cfe3435db4f0f3a7bd6acf60911c7f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-01-11 22:49:19 +0600

    get Employee route

--- http-client/http-rest-client.go
+++ http-client/http-rest-client.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"gopkg.in/resty.v1"
+)
+
 const (
 	CONN_HOST = "localhost"
 	CONN_PORT = "8090"
@@ -7,8 +15,20 @@ const (
 
 const WEB_SERVICE_HOST string = "http://localhost:8080"
 
-type employee struct {
+type Employee struct {
 	Id        string `json:"id"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
+}
+
+func getEmployee(w http.ResponseWriter, r *http.Request) {
+	response, err := resty.R().Get(WEB_SERVICE_HOST + "/employee")
+	if err != nil {
+		log.Printf("Error getting data from the webservice %s::", err)
+		return
+	}
+
+	print(response, err)
+	fmt.Fprint(w, response.String())
+
 }
