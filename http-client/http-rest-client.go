ac37ed0455fe6a7027aecf475003a8a0bc98f6da
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-01-12 19:02:49 +0600

    addEmployee route handler

--- http-client/http-rest-client.go
+++ http-client/http-rest-client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -35,7 +36,30 @@ func getEmployees(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func addEmployee(w http.ResponseWriter, r *http.Request) {
+	employee := Employee{}
+	decodingErr := json.NewDecoder(r.Body).Decode(&employee)
+
+	if decodingErr != nil {
+		log.Print("Error occured while decoidng employee", decodingErr)
+		return
+	}
+
+	log.Print("adding employee id ::", employee.Id, employee.FirstName, employee.LastName)
+	response, err := resty.R().SetHeader("Content-Type", "application/json").SetBody(Employee{Id: employee.Id, FirstName: employee.FirstName, LastName: employee.LastName}).Post(WEB_SERVICE_HOST + "/employee/add")
+	if err != nil {
+		log.Print("error occured while adding employee ::", err)
+		return
+	}
+
+	print(response, err)
+	fmt.Print(w, response.String())
+
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(false)
 	router.HandleFunc("/employees", getEmployees).Methods("GET")
+	employee := router.PathPrefix("/employee").Subrouter()
+	employee.HandleFunc("/add", addEmployee).Methods("POST")
 }
