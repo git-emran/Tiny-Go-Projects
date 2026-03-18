81be8ed97b10bd6da3fc5d4f02f9cc1b6d660f26
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-03-18 10:17:16 +0600

    setting up the initial employee type struct and data

--- angular-client/angular-client/server.go
+++ angular-client/angular-client/server.go
@@ -1,6 +1,51 @@
 package main
 
+import "net/http"
+
 const (
 	CONN_HOST = "localhost"
 	CONN_PORT = "8080"
 )
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"getEmployees",
+		"GET",
+		"/employees",
+		getEmployees,
+	},
+
+	Route{
+		"addEmployee",
+		"POST",
+		"/employee/add",
+		addEmployee,
+	},
+}
+
+type Employee struct {
+	Id        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type Employees []Employee
+
+var employees []Employee
+
+func init() {
+	employees = Employees{
+		Employee{Id: "1", FirstName: "Uno", LastName: "Momento"},
+		Employee{Id: "2", FirstName: "Dos", LastName: "Loco"},
+	}
+
+}
--- angular-client/angular-client/src/app/models/employee.ts
+++ angular-client/angular-client/src/app/models/employee.ts
@@ -5,3 +5,4 @@ export class Employee {
     public lastName: string,
   ) {}
 }
+
