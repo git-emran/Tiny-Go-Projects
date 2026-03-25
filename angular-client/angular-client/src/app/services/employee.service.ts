d8542c674154fe7f86a504d78b97b358b66b90c2
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-03-25 21:30:14 +0600

    angular getEmployee client

--- angular-client/angular-client/src/app/services/employee.service.ts
+++ angular-client/angular-client/src/app/services/employee.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Employee } from '../models/employee';
+import { error } from 'console';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class EmployeeService {
+  private baseUrl = 'http://localhost:8080';
   constructor(private http: HttpClient) {}
+  getEmployees(): Observable<Employee[]> {
+    return this.http
+      .get<Employee[]>(`${this.baseUrl}/employees`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error(error);
+    return throwError(() => error.message || 'server error');
+  }
 }
