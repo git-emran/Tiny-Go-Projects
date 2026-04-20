3f0f11c2c935fd17aa4b7ffb5cfcad1136bd8c7f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-04-20 20:25:12 +0600

    minor changes

--- mysql-db-go/connect-mysql.go
+++ mysql-db-go/connect-mysql.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+)
+
 const (
 	Host           = "localhost"
 	Port           = "8080"
 	driver         = "mysql"
 	dataSourceName = "root:@/mydb "
 )
+
+var db *sql.DB
+var connectionError error
+
+func init() {
+	db, connectionError = sql.Open(driver, dataSourceName)
+
+	if connectionError != nil {
+		log.Fatal("error connecting to Database::", connectionError)
+	}
+}
