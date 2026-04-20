8b48bb9bd07d06366320b4cc4a028e3fad1989c2
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-04-20 21:50:23 +0600

    connecting and reading from the mysql server

--- mysql-db-go/connect-mysql.go
+++ mysql-db-go/connect-mysql.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
 	Host           = "localhost"
 	Port           = "8080"
 	driver         = "mysql"
-	dataSourceName = "root:@/mydb "
+	dataSourceName = "root:@/mydb"
 )
 
 var db *sql.DB
@@ -20,5 +24,32 @@ func init() {
 
 	if connectionError != nil {
 		log.Fatal("error connecting to Database::", connectionError)
+	}
+}
+
+func getCurrentDB(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT DATABASE() as db")
+	if err != nil {
+		log.Print("Error executing query::", err)
+		return
+	}
+	defer rows.Close()
+
+	var db string
+	for rows.Next() {
+		rows.Scan(&db)
+	}
+
+	fmt.Fprintf(w, "Current database is:: %s", db)
+}
+
+func main() {
+	http.HandleFunc("/", getCurrentDB)
+	defer db.Close()
+	err := http.ListenAndServe(Host+":"+Port, nil)
+
+	if err != nil {
+		log.Fatal("Error starting http server ::", err)
+		return
 	}
 }
