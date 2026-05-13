4b315758592c74811e489942c84625637edc48fa
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-13 19:27:32 +0600

    job dispatching without queue

--- http-image-generator-api/main.go
+++ http-image-generator-api/main.go
@@ -2,46 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"math/rand"
+	"time"
 )
 
-type Message struct {
-	Name  string `json:"name"`
-	Email string `json:"email" binding:"required, email"`
+func sleepSomeTime() string {
+	sleepTime := time.Duration(rand.Intn(60)) * time.Second
+	message := fmt.Sprintf("%s\n", sleepTime)
+	time.Sleep(sleepTime)
+	return message
 }
 
-func router() *gin.Engine {
-	r := gin.Default()
-	userRoute := r.Group("/user")
-	userRoute.GET("/hello/:name", func(c *gin.Context) {
-		user := c.Param("name")
-		response := fmt.Sprintf("Hello, %s", user)
-		c.String(http.StatusOK, response)
-	})
-
-	userRoute.POST("/post", func(c *gin.Context) {
-		body := Message{}
-		if err := c.BindJSON(&body); err != nil {
-			c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-		fmt.Println(body)
-		c.JSON(http.StatusAccepted, &body)
-	})
-
-	userRoute.POST("/upload", func(c *gin.Context) {
-		file, _ := c.FormFile("file")
-		log.Println(file.Filename)
-		c.SaveUploadedFile(file, "/tmp/tempfile")
-		c.String(http.StatusOK, fmt.Sprintf("%s uploaded!", file.Filename))
-	})
-
-	return r
+func job(i int, rets chan string) {
+	sleepSomeTime()
+	rets <- fmt.Sprintf("Hi gopher, handle the job: %02d", +i)
 }
 
 func main() {
-	router().Run()
+	taskN := 100
+	rets := make(chan string, taskN)
+	for i := 0; i < taskN; i++ {
+		go job(i, rets)
+	}
+
+	for i := 0; i < taskN; i++ {
+		fmt.Println("message:", <-rets)
+		time.Sleep(20 * time.Millisecond)
+	}
 }
