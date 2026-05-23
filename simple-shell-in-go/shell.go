e182c22b8294eaa775f4d69d6875616a87051a5b
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-23 12:00:21 +0600

    Reading keyboard input: Shell command 'ls'

---
+++ simple-shell-in-go/shell.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		// read the keyboard input
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		// handling the execution of input
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func execInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+	cmd := exec.Command(input)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
