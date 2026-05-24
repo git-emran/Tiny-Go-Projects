5bb5dbeeab55e9cd7fcf01caa97c144c6696674f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-24 18:25:41 +0600

    Todo functions and unit tests

---
+++ cli-todo/go.mod
@@ -0,0 +1,3 @@
+module github.com/git-emran/tiny-go-projects/cli-todo
+
+go 1.25.0
---
+++ cli-todo/todo_test.go
@@ -0,0 +1,95 @@
+package clitodo_test
+
+import (
+	"os"
+	"testing"
+
+	clitodo "github.com/git-emran/tiny-go-projects/cli-todo"
+)
+
+func TestAdd(t *testing.T) {
+	l := clitodo.List{}
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
+	}
+}
+
+func TestComplete(t *testing.T) {
+	l := clitodo.List{}
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q got %q instead", taskName, l[0].Task)
+	}
+
+	if l[0].Done {
+		t.Errorf("New Task should not be completed")
+	}
+	l.Complete(1)
+
+	if !l[0].Done {
+		t.Errorf("New Task should be completed")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	l := clitodo.List{}
+
+	tasks := []string{
+		"New Task 1",
+		"New Task 2",
+		"New Task 3",
+	}
+
+	for _, v := range tasks {
+		l.Add(v)
+	}
+
+	if l[0].Task != tasks[0] {
+		t.Errorf("Expected %q got %q instead", tasks[0], l[0].Task)
+	}
+
+	l.Delete(2)
+
+	if len(l) != 2 {
+		t.Errorf("Expected list length %d, got %d instead.", 2, len(l))
+	}
+
+	if l[1].Task != tasks[2] {
+		t.Errorf("Expected %q, got %q instead.", tasks[2], l[1].Task)
+	}
+}
+
+func TestSaveGet(t *testing.T) {
+	l1 := clitodo.List{}
+	l2 := clitodo.List{}
+
+	taskName := "New Task"
+	l1.Add(taskName)
+
+	if l1[0].Task != taskName {
+		t.Errorf("Expected %q got %q instead", taskName, l1[0].Task)
+	}
+	testfile, err := os.CreateTemp("", "")
+	if err != nil {
+		t.Errorf("Error creating temp file: %s", err)
+	}
+
+	defer os.Remove(testfile.Name())
+
+	if err := l1.Save(testfile.Name()); err != nil {
+		t.Fatalf("Error saving list to file: %s", err)
+	}
+
+	if err := l2.Get(testfile.Name()); err != nil {
+		t.Fatalf("Error saving list to file: %s", err)
+	}
+
+	if l1[0].Task != l2[0].Task {
+		t.Errorf("Task %q should match %q task", l1[0].Task, l2[0].Task)
+	}
+}
