e05d2f6a699f123795a06181c3789d19f987b63e
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-27 22:39:08 +0600

    Reading the content of the input Markdown file into slice of byte

--- cli-markdown-preview/main.go
+++ cli-markdown-preview/main.go
@@ -1,18 +1,52 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"text/template/parse"
+
+	"gioui.org/io/input"
+)
+
 const (
 	header = `<!DOCTYPE html>
 	<html>
 		<head>
 			<meta http-equiv="content-type" content="text/html; charset=utf-8">
 			<title>Markdown Preview Tool</title>
-
 		</head>
 		<body>
-
-		</body>
 		`
 	footer = `
+			</body>
 	</html>
 	`
 )
+
+func main() {
+	filename := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string) error {
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	htmlData := parseContent(input)
+	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
+	fmt.Println(outName)
+	return saveHTML(outName, htmlData)
+}
