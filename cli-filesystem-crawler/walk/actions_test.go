c1384d708edc0258e7fb30b982a8791a03118c56
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-08-20 21:58:57 +0600

    actions test and lexer tst

--- cli-filesystem-crawler/walk/actions_test.go
+++ cli-filesystem-crawler/walk/actions_test.go
@@ -1,6 +1,9 @@
 package walk
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestFilterOut(t *testing.T) {
 	testCases := []struct {
@@ -11,6 +14,24 @@ func TestFilterOut(t *testing.T) {
 		expected bool
 	}{
 		{"FilterNoExtension", "testdata/dir.log", "", 0, false},
-		{"FilterNoExtensionMatch", "testdata/dir.log", "", 0, false},
+		{"FilterExtensionMatch", "testdata/dir.log", ".log", 0, false},
+		{"FilterExtensionNoMatch", "testdata/dir.log", ".sh", 0, true},
+		{"FilterExtensionSizeMatch", "testdata/dir.log", ".log", 10, false},
+		{"FilterExtensionSizeNoMatch", "testdata/dir.log", ".log", 20, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			info, err := os.Stat(tc.file)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			f := FilterOut(tc.file, tc.ext, tc.minSize, info)
+
+			if f != tc.expected {
+				t.Errorf("Expected '%t'. got '%t' instead\n", tc.expected, f)
+			}
+		})
 	}
 }
--- interpreter-in-go/lexer/lexer_test.go
+++ interpreter-in-go/lexer/lexer_test.go
@@ -1,7 +1,14 @@
 package lexer
 
-import "testing"
+import (
+	"go/token"
+	"testing"
+)
 
 func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{}
 }
