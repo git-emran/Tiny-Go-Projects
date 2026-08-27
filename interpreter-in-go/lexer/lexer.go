2f5fbdf5b71e251e2519c148044b499ddf900239
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-08-27 17:14:35 +0600

    Preparing Lexer

---
+++ interpreter-in-go/lexer/lexer.go
@@ -0,0 +1,29 @@
+package lexer
+
+import "github.com/git-emran/tiny-go-projects/interpreter-in-go/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+}
--- interpreter-in-go/lexer/lexer_test.go
+++ interpreter-in-go/lexer/lexer_test.go
@@ -15,5 +15,14 @@ func TestNextToken(t *testing.T) {
 		{token.ASSIGN, "="},
 		{token.PLUS, "+"},
 		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+	for i, tt := range tests {
 	}
 }
