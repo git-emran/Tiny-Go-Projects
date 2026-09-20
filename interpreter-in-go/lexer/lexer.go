4f07eaf15f1ad19c98367043ccdcac79548b62d7
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-09-20 21:07:07 +0600

    feat: lexer implementation

--- interpreter-in-go/lexer/lexer.go
+++ interpreter-in-go/lexer/lexer.go
@@ -1,3 +1,4 @@
+// Package lexer handles all the lexical issues
 package lexer
 
 import "github.com/git-emran/tiny-go-projects/interpreter-in-go/token"
@@ -26,4 +27,32 @@ func (l *Lexer) readChar() {
 }
 
 func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
